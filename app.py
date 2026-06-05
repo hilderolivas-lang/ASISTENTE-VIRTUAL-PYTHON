d7d5c96ad4726e567c05ef7b552865c5ab8bfad4
Hilder olivas <hilder.olivas@ucn.edu.ni>
2026-06-05 11:25:11 -0600

    Organizador de archivos comando de voz

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # =====================================================
 # ASISTENTE INTELIGENTE - VERSION 1
-# Autor: Hilder
+# Autor: Hilder olivas
 #
 # Funciones:
 # - Reconocimiento de voz
@@ -24,8 +24,12 @@ import edge_tts
 import pygame
 import os
 
+#NUEVAS LIBRERIAS PARA MAEJO DE ARCHIVOS INVERTIGAR LO QUE HACER CADA UNA 
+import shutil
+from pathlib import Path
+
 # =====================================================
-# CREACION DE LA APLICACION FLASK
+# CREACION DE LA APLICACION FLASK: NOS SERVIRA PARA PUBLICARLA 
 # =====================================================
 
 app = Flask(__name__)
@@ -39,7 +43,8 @@ nombre_usuario = ""
 conversacion = []
 
 # =====================================================
-# FUNCION DE VOZ NATURAL
+# FUNCION DE VOZ NATURAL: ESTA VOZ SE HA CAMBIADO POR LO QUE SE ESCUCHABA MUY ROBOTICA EN EL ARCHIVO PT.PY ESTA 
+# OTRA VOZ QUE PUEDEN UTILIZAR SI LES GUSTA. OJO VERIFICAR SI ESTA FUNCIONANDO EL NARRADO EN WINDOWS
 # =====================================================
 
 async def generar_voz(texto):
@@ -68,7 +73,7 @@ async def generar_voz(texto):
         os.remove(archivo)
 
 # =====================================================
-# HABLAR
+#FUNCIO DE  HABLAR
 # =====================================================
 
 def hablar(texto):
@@ -91,7 +96,7 @@ def hablar_async(texto):
     hilo.start()
 
 # =====================================================
-# ESCUCHAR MICROFONO
+# ESCUCHAR MICROFONO DE MI LAPTON SI LO COLOCAMOS EN WEB O EN LINEA NO FUNCIONARA NO USA AUN MICROFONO DE NAVEGADOR
 # =====================================================
 
 def escuchar():
@@ -122,8 +127,165 @@ def escuchar():
 
     return texto.lower()
 
+
 # =====================================================
-# PROCESAR COMANDO
+# ORGANIZAR ARCHIVOS
+# =====================================================
+
+def organizar_archivos(carpeta):
+
+    try:
+
+        # ------------------------------------------
+        # OBTENER RUTA
+        # ------------------------------------------
+
+        usuario = os.getlogin()
+
+        if carpeta == "Descargas":
+
+            ruta = os.path.join(
+        "C:\\Users",
+        usuario,
+        "Downloads"
+    )
+
+        elif carpeta == "Escritorio":
+
+         ruta = os.path.join(
+        "C:\\Users",
+        usuario,
+        "Desktop"
+    )
+
+        elif carpeta == "Archivos":
+
+         ruta = r"D:\Escritorio\ARCHIVOS"
+
+        else:
+
+            return "No encontré la carpeta."
+            
+        # ------------------------------------------
+        # CREAR CARPETAS DESTINO
+        # ------------------------------------------
+
+        pdf_dir = os.path.join(ruta, "PDF")
+        imagenes_dir = os.path.join(ruta, "Imagenes")
+        videos_dir = os.path.join(ruta, "Videos")
+        otros_dir = os.path.join(ruta, "Otros")
+
+        os.makedirs(pdf_dir, exist_ok=True)
+        os.makedirs(imagenes_dir, exist_ok=True)
+        os.makedirs(videos_dir, exist_ok=True)
+        os.makedirs(otros_dir, exist_ok=True)
+
+        contador = 0
+
+        # ------------------------------------------
+        # RECORRER ARCHIVOS
+        # ------------------------------------------
+
+        for archivo in os.listdir(ruta):
+
+            archivo_completo = os.path.join(
+                ruta,
+                archivo
+            )
+
+            # Ignorar carpetas
+
+            if os.path.isdir(archivo_completo):
+
+                continue
+
+            extension = Path(
+                archivo
+            ).suffix.lower()
+
+            # --------------------------------------
+            # PDF
+            # --------------------------------------
+
+            if extension == ".pdf":
+
+                destino = os.path.join(
+                    pdf_dir,
+                    archivo
+                )
+
+            # --------------------------------------
+            # IMAGENES
+            # --------------------------------------
+
+            elif extension in [
+
+                ".jpg",
+                ".jpeg",
+                ".png",
+                ".gif",
+                ".bmp",
+                ".webp"
+
+            ]:
+
+                destino = os.path.join(
+                    imagenes_dir,
+                    archivo
+                )
+
+            # --------------------------------------
+            # VIDEOS
+            # --------------------------------------
+
+            elif extension in [
+
+                ".mp4",
+                ".avi",
+                ".mov",
+                ".mkv"
+
+            ]:
+
+                destino = os.path.join(
+                    videos_dir,
+                    archivo
+                )
+
+            # --------------------------------------
+            # ACTIVIDAD DEL ESTUDIANTE
+            # Agregar Word, Excel, ZIP,
+            # PowerPoint y Python
+            # --------------------------------------
+
+            else:
+
+                destino = os.path.join(
+                    otros_dir,
+                    archivo
+                )
+
+            shutil.move(
+                archivo_completo,
+                destino
+            )
+
+            contador += 1
+
+        return (
+            f"Se organizaron correctamente "
+            f"{contador} archivos."
+        )
+    except Exception as e:
+
+     return (
+            f"Ocurrió un error: {str(e)}"
+        )
+
+
+
+# =====================================================
+# PROCESAR COMANDO : EL QUE YO LE DICTE O HABLE
 # =====================================================
 
 def procesar_comando(comando):
@@ -139,9 +301,14 @@ def procesar_comando(comando):
         nombre_usuario = comando.title()
 
         return (
-            f"Mucho gusto {nombre_usuario}. "
-            f"¿Qué deseas hacer?"
-        )
+        f"Mucho gusto {nombre_usuario}. "
+        f"Estoy listo para ayudarte.\n"
+        f"Puedes decir:\n"
+        f"Organizar archivos,\n"
+        f"Dictar texto,\n"
+        f"Tomar fotografía,\n"
+        f"O salir."
+    )
 
     # -------------------------------------------------
     # OPCION DICTADO
@@ -159,10 +326,10 @@ def procesar_comando(comando):
 
     elif "organizar" in comando:
 
-        return (
-            "Has seleccionado organizar archivos."
+        respuesta=organizar_archivos (
+            "Archivos"
         )
-
+        return respuesta
     # -------------------------------------------------
     # OPCION FOTO
     # -------------------------------------------------
@@ -194,7 +361,7 @@ def procesar_comando(comando):
         )
 
 # =====================================================
-# PAGINA PRINCIPAL
+# PAGINA PRINCIPAL, ESTO ES PARA UTILIZAR MI HTML CON EL ASISTENTE
 # =====================================================
 
 @app.route("/", methods=["GET", "POST"])
@@ -261,3 +428,4 @@ if __name__ == "__main__":
     app.run(
         debug=False
     )
+    
